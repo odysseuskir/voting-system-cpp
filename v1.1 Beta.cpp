0146e3580fa091ad48d66e09f972e7a6c55a92f2
Odysseus Kirikopoulos <odysseuskirikopoulos@gmail.com>
2022-08-02 09:22:56 +0300

    Update and rename 1.0_beta.cpp to v1.1 Beta.cpp

--- v1.1 Beta.cpp
+++ v1.1 Beta.cpp
@@ -1,6 +1,8 @@
-// Author: Odysseus-Abraham Kirikopoulos
-// (CC) Attribution-NonCommercial 4.0 International - SOME RIGHTS RESERVED - ODYSSEUS ABRAHAM KIRIKOPOULOS - 2022
-// Version: 1.0 beta
+/*
+Author: Odysseus-Abraham Kirikopoulos
+License: GNU General Public License v3.0
+Version: 1.1 beta
+*/
 
 #include <iostream>
 
@@ -16,21 +18,10 @@ int main() {
 
     std::cout << "Launching\n";
     std::cout << "Author: Odysseus-Abraham Kirikopoulos\n";
-    std::cout << "(CC) Attribution-NonCommercial 4.0 International - SOME RIGHTS RESERVED - ODYSSEUS ABRAHAM KIRIKOPOULOS - 2022\n";
-    std::cout << "Build Version: 0.1 dev\n";
+    std::cout << "License: GNU General Public License v3.0\n";
+    std::cout << "Build Version: 1.1 beta\n";
 
     std::cout << "Welcome to the Voting System";
-
-    // while(authentic_copy == false) {
-    //     std::cout << "\nPlease enter the election center authetication code: ";
-    //     std::cin >> authentication;
-    //     if (authentication == "demo") {
-    //         authentic_copy = true;
-    //         std::cout << "You have successfuly signed in";
-    //     } else {
-    //         std::cout << "Failed to authenticate. Please try again.";
-    //     }
-    // }
 
     std::cout << "\nParty list:\n\nParty A (Code: 1)\nParty B (Code: 2)\nParty C (Code: 3)\n\nTo exit submitions type 0\n";
 
@@ -61,4 +52,12 @@ int main() {
     std::cout << "\nParty A has: " << party_a << " votes.\n";
     std::cout << "Party B has: " << party_b << " votes.\n";
     std::cout << "Party C has: " << party_c << " votes.\n";
+
+    if (party_a > party_b && party_a > party_c) {
+        std::cout << "\nParty A has the most votes";
+    } else if (party_b > party_a && party_b > party_c) {
+        std::cout << "\nParty B has the most votes";
+    } else if (party_c > party_a && party_c > party_b) {
+        std::cout << "\nParty C has the most votes";
+    }
 }
