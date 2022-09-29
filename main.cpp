4130600833e3ea201e09c16cccb8b10487b1c143
Odysseus-Abraham Kirikopoulos <odysseuskirikopoulos@gmail.com>
2022-09-29 23:09:12 +0300

    Added comments and updated license

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
--/*
-Author: Odysseus-Abraham Kirikopoulos
+/*Author: Odysseus-Abraham Kirikopoulos
 License: GNU General Public License v3.0
-Version: 1.0 Stable
+Version: 1.1 Stable
 */
 
 #include <iostream>
 
-int main() {
+void main() {
 
+    // Declare variables
     double party_a = 0.0;
     double party_b = 0.0;
     double party_c = 0.0;
@@ -19,18 +19,28 @@ int main() {
     double party_b_percentage = 0.0;
     double party_c_percentage = 0.0;
 
+    // Print license and version
+
+    std::cout << "Voting System Copyright (C) 2022 Odysseus-Abraham Kirikopoulos\nThis program comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it under certain conditions.";
     std::cout << "Author: Odysseus-Abraham Kirikopoulos\n";
-    std::cout << "License: GNU General Public License v3.0\n";
-    std::cout << "Build Version: 1.0 Stable\n";
+    std::cout << "Build Version: 1.1 Stable\n";
+
+    // Print welcome message
 
     std::cout << "Welcome to the Voting System";
+
+    // Print party list
 
     std::cout << "\nParty list:\n\nParty A (Code: 1)\nParty B (Code: 2)\nParty C (Code: 3)\n\nTo exit submitions type 0\n";
 
     while (option != 0) {
 
+        // Print vote submition
+
         std::cout << "Vote here: ";
         std::cin >> option;
+
+        // Calculate votes
 
         if (option == 1) {
         party_a += 1.0;
@@ -51,6 +61,8 @@ int main() {
         }
     }
 
+    // Calculate percentages
+
     if (party_a == 0 && party_b == 0 && party_c == 0) {
         party_a_percentage = 0;
         party_b_percentage = 0;
@@ -60,6 +72,8 @@ int main() {
         party_b_percentage = (party_b / ttl_votes) * 100.0;
         party_c_percentage = (party_c / ttl_votes) * 100.0;
     }
+
+    // Print results
 
     std::cout << "\nParty A has: " << party_a << " votes and " << party_a_percentage << "%.\n";
     std::cout << "Party B has: " << party_b << " votes and " << party_b_percentage << "%.\n";
